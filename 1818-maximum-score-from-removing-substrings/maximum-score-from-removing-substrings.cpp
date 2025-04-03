f1ce95bc3dd45adf633664588fb473d546ff17ad
NeerajKumar1561 <127119870+nk221561@users.noreply.github.com>
2025-04-03 10:17:37 +0530

    Time: 15 ms (94.58%) | Memory: 26.9 MB (63.14%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+       vector<char> st1;
+       stack<char> st2;
+       char first = (x>y) ? 'a' : 'b';
+       char second = (x>y) ? 'b' : 'a';
+       int maxV= max(x, y);
+       int minV = min(x, y);
+       int ans=0;
+        for(char c: s){
+            if(!st1.empty()  &&  st1.back()==first && c== second){
+                ans= ans+ maxV;
+                st1.pop_back();
+            }
+            else{
+                     st1.push_back(c);
+            }
+           
+
+        }
+
+       
+
+        for(char c: st1){
+            if(!st2.empty()  &&  st2.top()==second && c== first){
+                ans= ans+ minV;
+                st2.pop();
+            }
+            else{
+                 st2.push(c);
+            }
+
+
+        }
+
+        return ans;
+
+       }
+    
+};
