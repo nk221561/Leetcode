f795e4d056682c106b089724fe4b23b6843ccef9
NeerajKumar1561 <127119870+nk221561@users.noreply.github.com>
2025-04-19 10:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.49%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -19,20 +19,20 @@ public:
         
         vector<int> ans;
         while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            
-            for(auto it: adj[node]) {
-                indegree[it]--;
-                if(indegree[it] == 0) {
-                    q.push(it);
-                }
+           int node= q.front();
+           q.pop();
+           ans.push_back(node);
+           for(auto it : adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0){
+                q.push(it);
             }
+           }
+
         }
                
   
         
-    return ans.size() == numCourses;    
+     return ans.size()== numCourses;
     }
 };
